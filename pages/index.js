542a5a46a226c3ebd8bf82a4646342b251b8868c
Frank Basham <frankabasham@gmail.com>
2022-12-30 11:20:23 -0500

    add colour change and brush change

--- pages/index.js
+++ pages/index.js
@@ -2,26 +2,70 @@ import { fabric } from "fabric";
 import { useState, useEffect, useRef } from "react";
 
 export default function Home() {
-  let [canvas, setCanvas] = useState();
   const canvasRef = useRef();
+  let [canvas, setCanvas] = useState();
+  let [colour, setColour] = useState("black");
+  let [brush, setBrush] = useState(2);
 
   useEffect(() => {
-    let C = new fabric.Canvas("canvas", {
+    let canvas = new fabric.Canvas("canvas", {
       height: 400,
       width: 600,
       isDrawingMode: true,
     });
-    C.freeDrawingBrush.width = 5;
-    C.freeDrawingBrush.color = "red";
-    setCanvas(C);
+    canvas.freeDrawingBrush.width = brush;
+    canvas.freeDrawingBrush.color = colour;
+    setCanvas(canvas);
+
+    return () => {
+      if (canvas) canvas.dispose();
+      canvas = undefined;
+    };
   }, []);
 
+  useEffect(() => {
+    if (!canvas) return;
+    canvas.freeDrawingBrush.width = brush;
+  }, [brush]);
+
+  useEffect(() => {
+    if (!canvas) return;
+    canvas.freeDrawingBrush.color = colour;
+  }, [colour]);
+
+  function handleColourChange(e) {
+    setColour(e.target.value);
+    canvas.freeDrawingBrush.color = e.target.value;
+  }
+
+  function handleBrushChange(e) {
+    setBrush(e.target.value);
+    canvas.freeDrawingBrush.width = e.target.value;
+  }
+
   return (
-    <>
-      <canvas ref={canvasRef} id="canvas"></canvas>
-      <button onClick={() => console.log(canvasRef.current.toDataURL())}>
+    <div className="flex flex-col justify-center items-center">
+      <div>
+        <canvas ref={canvasRef} id="canvas" className="border-2"></canvas>
+        <input
+          onChange={handleColourChange}
+          type="color"
+          className="w-8 h-9"
+        ></input>
+        <input
+          type="range"
+          min={0}
+          max={30}
+          onChange={handleBrushChange}
+          defaultValue={brush}
+        />
+      </div>
+      <button
+        onClick={() => console.log(canvasRef.current.toDataURL())}
+        className="bg-blue-700 text-white py-1 px-2 rounded hover:bg-blue-900"
+      >
         export
       </button>
-    </>
+    </div>
   );
 }
